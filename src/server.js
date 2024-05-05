f0811194a9ac207226d4fb3ff6a4aaccf8f80541
Marcelo <marcelosouzacorrea@hotmail.com>
2024-05-04 21:15:31 -0400

    a

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
 require('dotenv').config({path:'variaveis.env'})
-const exports = require('express')
+const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
 const routes = require('./routes')
 
-const server = exports();
+const server = express();
 server.use(cors())
 server.use(bodyParser.urlencoded({extended: false}))
 
 server.listen(process.env.PORT, ()=>{
-    console.log('Servidor rodando em http://localhost:${}')
+    console.log(`Servidor rodando em http://localhost:${process.env.PORT}`)
 })
