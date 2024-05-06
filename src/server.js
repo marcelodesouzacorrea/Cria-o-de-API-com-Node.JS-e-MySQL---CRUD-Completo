7ed1f043bb8a88cc8567e785496ef5d8f77e868c
Marcelo <marcelosouzacorrea@hotmail.com>
2024-05-05 22:12:29 -0400

    a

--- src/server.js
+++ src/server.js
@@ -9,6 +9,8 @@ const server = express();
 server.use(cors())
 server.use(bodyParser.urlencoded({extended: false}))
 
+server.use('/api', routes)
+
 server.listen(process.env.PORT, ()=>{
     console.log(`Servidor rodando em http://localhost:${process.env.PORT}`)
 })
